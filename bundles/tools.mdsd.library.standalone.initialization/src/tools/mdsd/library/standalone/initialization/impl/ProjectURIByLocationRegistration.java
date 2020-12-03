67e9355d38177b6ecd5ae86aebb1dfb91123c3f8
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-12-03 12:54:06 +0100

    Considered review comments.

--- bundles/tools.mdsd.library.standalone.initialization/src/tools/mdsd/library/standalone/initialization/impl/ProjectURIByLocationRegistration.java
+++ bundles/tools.mdsd.library.standalone.initialization/src/tools/mdsd/library/standalone/initialization/impl/ProjectURIByLocationRegistration.java
@@ -5,30 +5,20 @@ import java.io.File;
 import org.eclipse.emf.common.util.URI;
 
 import tools.mdsd.library.standalone.initialization.InitializationTask;
-import tools.mdsd.library.standalone.initialization.StandaloneInitializationException;
 
 /**
- * Registers URIs in the EMF platform for a given project. After the
- * registration, platform resource and platform plugin URIs can properly be
- * resolved for this project. The location of the project has to be given.
+ * Registers URIs in the EMF platform for a given project. After the registration, platform resource
+ * and platform plugin URIs can properly be resolved for this project. The location of the project
+ * has to be given.
  */
-public class ProjectURIByLocationRegistration implements InitializationTask {
+public class ProjectURIByLocationRegistration extends InitializationTaskWrapper {
 
-	private final InitializationTask delegate;
+    public ProjectURIByLocationRegistration(File projectRootLocation, String projectName) {
+        super(createDelegate(projectRootLocation, projectName));
+    }
 
-	public ProjectURIByLocationRegistration(File projectRootLocation, String projectName) {
-		this.delegate = new ProjectURIByURIRegistration(URI.createFileURI(projectRootLocation.getAbsolutePath()),
-				projectName);
-	}
-
-	@Override
-	public void initilizationWithoutPlatform() throws StandaloneInitializationException {
-		delegate.initilizationWithoutPlatform();
-	}
-
-	@Override
-	public void initializationWithPlatform() throws StandaloneInitializationException {
-		delegate.initializationWithPlatform();
-	}
+    protected static InitializationTask createDelegate(File projectRootLocation, String projectName) {
+        return new ProjectURIByURIRegistration(URI.createFileURI(projectRootLocation.getAbsolutePath()), projectName);
+    }
 
 }
--- bundles/tools.mdsd.library.standalone.initialization/src/tools/mdsd/library/standalone/initialization/impl/ProjectURIByURIRegistration.java
+++ bundles/tools.mdsd.library.standalone.initialization/src/tools/mdsd/library/standalone/initialization/impl/ProjectURIByURIRegistration.java
@@ -1,5 +1,7 @@
 package tools.mdsd.library.standalone.initialization.impl;
 
+import java.util.Optional;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.impl.URIMappingRegistryImpl;
@@ -8,34 +10,37 @@ import tools.mdsd.library.standalone.initialization.InitializationTask;
 import tools.mdsd.library.standalone.initialization.StandaloneInitializationException;
 
 /**
- * Registers URIs in the EMF platform for a given project. After the
- * registration, platform resource and platform plugin URIs can properly be
- * resolved for this project. The location of the project is given by a {@link URI},
- * which should obviously not be just a platform URI.
+ * Registers URIs in the EMF platform for a given project. After the registration, platform resource
+ * and platform plugin URIs can properly be resolved for this project. The location of the project
+ * is given by a {@link URI}, which should obviously not be just a platform URI.
  */
 public class ProjectURIByURIRegistration implements InitializationTask {
 
-	private final URI realProjectURI;
-	private final String projectName;
+    private final Optional<URI> realProjectURI;
+    private final String projectName;
 
-	/**
-	 * Constructs the registration task.
-	 * 
-	 * @param realProjectURI The {@link URI} specifying the real location of the
-	 *                       project.
-	 * @param projectName    The name of the project to be registered.
-	 */
-	public ProjectURIByURIRegistration(URI realProjectURI, String projectName) {
-		this.realProjectURI = realProjectURI;
-		this.projectName = projectName;
-	}
+    /**
+     * Constructs the registration task.
+     * 
+     * @param realProjectURI
+     *            The {@link URI} specifying the real location of the project.
+     * @param projectName
+     *            The name of the project to be registered.
+     */
+    public ProjectURIByURIRegistration(URI realProjectURI, String projectName) {
+        this.realProjectURI = Optional.ofNullable(realProjectURI);
+        this.projectName = projectName;
+    }
 
-	@Override
-	public void initilizationWithoutPlatform() throws StandaloneInitializationException {
-		EcorePlugin.getPlatformResourceMap().put(projectName, realProjectURI);
-		var pluginURI = URI.createPlatformPluginURI("/" + projectName + "/", false);
-		var platformURI = URI.createPlatformResourceURI("/" + projectName + "/", false);
-		URIMappingRegistryImpl.INSTANCE.put(pluginURI, platformURI);
-	}
+    @Override
+    public void initilizationWithoutPlatform() throws StandaloneInitializationException {
+        var projectURI = realProjectURI.orElseThrow(() -> new StandaloneInitializationException(
+                "Could not register project " + projectName + " because of missing URI."));
+        EcorePlugin.getPlatformResourceMap()
+            .put(projectName, projectURI);
+        var pluginURI = URI.createPlatformPluginURI("/" + projectName + "/", false);
+        var platformURI = URI.createPlatformResourceURI("/" + projectName + "/", false);
+        URIMappingRegistryImpl.INSTANCE.put(pluginURI, platformURI);
+    }
 
 }
